64c8f38149226cb4d5606e568053eabaad3dd2bd
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-01 16:13:22 -0700

    added app engine config and script

--- timeplot.py
+++ timeplot.py
@@ -17,7 +17,7 @@ data = []
 tdata = []
 mindata = []
 prev=None
-for f in range(24):
+for f in range(24*2):
   td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
   td = td + timedelta(minutes=f*30)
   print td
@@ -25,7 +25,8 @@ for f in range(24):
   if prev:
     tdata.append(td)
     mindata.append(gmaps.duration/60)
-    data.append(gmaps.duration_in_traffic/60-prev)
+#    data.append(gmaps.duration_in_traffic/60-prev)
+    data.append(gmaps.duration_in_traffic/60)
   prev = gmaps.duration_in_traffic/60
 
 print data
