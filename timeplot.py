0a3418cc5b17bb08c90be5f355b71e6b09d3821b
Rodney Peck <ropeck@ropeck-macbookpro.roam.corp.google.com>
2016-11-01 14:22:25 -0700

    checkin

--- timeplot.py
+++ timeplot.py
@@ -16,17 +16,20 @@ d=datetime.today()
 data = []
 tdata = []
 mindata = []
-for f in range(24*6):
+prev=None
+for f in range(24):
   td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
-  td = td + timedelta(minutes=f*10)
+  td = td + timedelta(minutes=f*30)
   print td
   directions_result = gmaps.directions(td)
-  tdata.append(td)
-  mindata.append(gmaps.duration/60)
-  data.append(gmaps.duration_in_traffic/60)
+  if prev:
+    tdata.append(td)
+    mindata.append(gmaps.duration/60)
+    data.append(gmaps.duration_in_traffic/60-prev)
+  prev = gmaps.duration_in_traffic/60
 
 print data
 #pl.plot_date(mindata, 'bs', data, 'g^')
-pl.plot_date(tdata, mindata, 'bs')
-pl.plot_date(tdata, data, 'g^')
+#pl.plot_date(tdata, mindata, 'bs')
+pl.plot_date(tdata, data, 'g^-')
 pl.show()
