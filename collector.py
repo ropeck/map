da5e891264ef315176f62e167c6f2b0377fca8e9
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-02 14:28:19 -0700

    checkin

--- collector.py
+++ collector.py
@@ -5,19 +5,9 @@ from datetime import datetime
 from datetime import timedelta
 import directions
 import logging
-import json
-from google.appengine.ext import ndb
 
 from flask import Flask
 
-class Mapdirection(ndb.Model):
-    arrive = ndb.DateProperty()
-    body = ndb.StringProperty(indexed=False)
-    delay = ndb.IntegerProperty()
-    depart = ndb.DateProperty()
-    origin = ndb.StringProperty()
-    destination = ndb.StringProperty()
-    duration = ndb.IntegerProperty()
 
 app = Flask(__name__)
 
@@ -27,19 +17,7 @@ def hello():
 
     gmaps = directions.Directions()
 
-
-    def val(d,key='duration_in_traffic'): 
-      return int(d[key]['value'],)
-
     r = gmaps.directions(datetime.now())
-
-    m = Mapdirection(depart=datetime.now(),
-                     origin=r['start_address'],
-                     destination=r['end_address'],
-                     body=json.dumps(r),
-                     duration=val(r,key='duration')/60,
-                     delay=(val(r) - val(r,key='duration'))/60)
-    m.put()
     return json.dumps(r)
 
 
--- whentogo.py
+++ whentogo.py
@@ -1,25 +1,49 @@
+# [START app]
 #!/usr/bin/env python
 from datetime import datetime
 from datetime import timedelta
 import directions
  
+from flask import Flask
+from cStringIO import StringIO
+import sys
 
-gmaps = directions.Directions()
 
-d=datetime.today()
-l = gmaps.directions(d)
-print gmaps.distance_text, 'normally', gmaps.duration_text
-print 'LEAVE ARRIVE NOTES'
-print '[now]',(d+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
-d=d.replace(d.year,d.month,d.day,d.hour,int(d.minute/10)*10,0,0) + timedelta(minutes=10)
-for f in range(24):
-#  td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
-  td = d
-  td = td + timedelta(minutes=10*f)
-  l = gmaps.directions(td)
-  print td.strftime("%H:%M"),(td+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
+app = Flask(__name__)
 
-    # 44 miles normally 0:52
-    # now   17:00 1:12 (+19)
-    # 16:50 17:09 1:12 (+19)
-    # ...
+
+@app.route('/whentogo')
+def whentogo():
+  gmaps = directions.Directions()
+
+  d=datetime.today()
+  l = gmaps.directions(d)
+
+  old_stdout = sys.stdout
+  sys.stdout = mystdout = StringIO()
+
+  print  gmaps.distance_text, 'normally', gmaps.duration_text
+  print 'LEAVE ARRIVE NOTES'
+  print '[now]',(d+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
+  d=d.replace(d.year,d.month,d.day,d.hour,int(d.minute/10)*10,0,0) + timedelta(minutes=10)
+  for f in range(24):
+  #  td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
+    td = d
+    td = td + timedelta(minutes=10*f)
+    l = gmaps.directions(td)
+    print td.strftime("%H:%M"),(td+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
+
+      # 44 miles normally 0:52
+      # now   17:00 1:12 (+19)
+      # 16:50 17:09 1:12 (+19)
+      # ...
+  sys.stdout = old_stdout
+  return mystdout.getvalue()
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+# [END app]
