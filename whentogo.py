2d910f2913ed7eb8815abdab7fa6340a52f90667
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-02 16:40:38 -0700

    fixed timezone in whentogo text

--- whentogo.py
+++ whentogo.py
@@ -8,7 +8,7 @@ import logging
 from flask import Flask
 from cStringIO import StringIO
 import sys
-
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -23,21 +23,22 @@ def whentogo():
   old_stdout = sys.stdout
   sys.stdout = mystdout = StringIO()
 
+  print "<pre>"
   print  gmaps.distance_text, 'normally', gmaps.duration_text
   print 'LEAVE ARRIVE NOTES'
   print '[now]',(d+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
   d=d.replace(d.year,d.month,d.day,d.hour,int(d.minute/10)*10,0,0) + timedelta(minutes=10)
   for f in range(24):
-  #  td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
-    td = d
-    td = td + timedelta(minutes=10*f)
+    td = d + timedelta(minutes=10*f)
     l = gmaps.directions(td)
-    print td.strftime("%H:%M"),(td+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
+    tz = td.replace(tzinfo=timezone("UTC")).astimezone(timezone("US/Pacific"))
+    print tz.strftime("%H:%M"),(tz+timedelta(minutes=gmaps.duration_in_traffic/60)).strftime("%H:%M"), gmaps.duration_in_traffic_text, gmaps.diffstr
 
       # 44 miles normally 0:52
       # now   17:00 1:12 (+19)
       # 16:50 17:09 1:12 (+19)
       # ...
+  print "</pre>"
   sys.stdout = old_stdout
   return mystdout.getvalue()
 
