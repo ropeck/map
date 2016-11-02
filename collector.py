4fe45b767e7ef0479721dcab6db3c7ac2ee6dd55
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-02 15:50:05 -0700

    collect times rounded to ten minutes to improve cache hits

--- collector.py
+++ collector.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from datetime import timedelta
 import directions
 import logging
+import json
 
 from flask import Flask
 
@@ -16,8 +17,11 @@ app = Flask(__name__)
 def hello():
 
     gmaps = directions.Directions()
-
-    r = gmaps.directions(datetime.now())
+    # to make the most useful cached data, we want to make the time rounded but app engine cron can run
+    # the script anytime.  So, zero out the end of the time to make it nice.
+    dt = datetime.now()
+    dt = dt.replace(dt.year,dt.month,dt.day,dt.hour,int(dt.minute/10)*10,0,0) + timedelta(minutes=10)
+    r = gmaps.directions(dt)
     return json.dumps(r)
 
 
