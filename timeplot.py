8b2a3a927a9159afeb104dc518bb1a7894581b84
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-02 17:49:20 -0700

    added plot

--- timeplot.py
+++ timeplot.py
@@ -7,30 +7,44 @@ import directions
 import numpy as np
 import pylab as pl
 
-gmaps = directions.Directions()
+import logging
 
+from flask import Flask
 
-d=datetime.today()
-#d=d.replace(d.year,d.month,31,0,0,0,0)
+app = Flask(__name__)
 
-data = []
-tdata = []
-mindata = []
-prev=None
-for f in range(24*2):
-  td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
-  td = td + timedelta(minutes=f*30)
-  print td
-  directions_result = gmaps.directions(td)
-  if prev:
-    tdata.append(td)
-    mindata.append(gmaps.duration/60)
-#    data.append(gmaps.duration_in_traffic/60-prev)
-    data.append(gmaps.duration_in_traffic/60)
-  prev = gmaps.duration_in_traffic/60
+@app.route('/plot')
+def plot():
+  gmaps = directions.Directions()
 
-print data
-#pl.plot_date(mindata, 'bs', data, 'g^')
-#pl.plot_date(tdata, mindata, 'bs')
-pl.plot_date(tdata, data, 'g^-')
-pl.show()
+  d=datetime.today()
+
+  data = []
+  tdata = []
+  mindata = []
+  prev=None
+  for f in range(24*2):
+    td = d.replace(d.year,d.month,d.day,d.hour,0,0,0) + timedelta(hours=1)
+    td = td + timedelta(minutes=f*30)
+    print td
+    directions_result = gmaps.directions(td)
+    if prev:
+      tdata.append(td)
+      mindata.append(gmaps.duration/60)
+  #    data.append(gmaps.duration_in_traffic/60-prev)
+      data.append(gmaps.duration_in_traffic/60)
+    prev = gmaps.duration_in_traffic/60
+
+  #pl.plot_date(mindata, 'bs', data, 'g^')
+  #pl.plot_date(tdata, mindata, 'bs')
+  pl.plot_date(tdata, data, 'g^-')
+
+#  pl.savefig(sys.stdout, format='png')
+
+  imgdata = StringIO.StringIO()
+  pl.savefig(imgdata)
+  imgdata.seek(0)  # rewind the data
+
+  print "Content-type: image/png\n"
+  uri = 'data:image/png;base64,' + urllib.quote(base64.b64encode(imgdata.buf))
+  print '<img src = "%s"/>' % uri
