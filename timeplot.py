d11cca30d20a479d49996a4c5c89a3bd4f823c09
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-09 15:52:48 -0800

    checkin

--- timeplot.py
+++ timeplot.py
@@ -178,7 +178,7 @@ def arrivedata(date):
   midnight = d.replace(d.year,d.month,d.day,8,0,0,0)
   data = [['Time', 'Drive', 'Delay']]
   for h in drawday(midnight, cache=False)[1:]:  # skip header
-    data.append([h[0], timeplus(h[0],h[2]), timeplus(h[0],h[1])])
+    data.append([h[0], timeplus(h[0],h[2]), timeplus(h[0],h[2]+h[1])])
 
   data = json.dumps(data)
   resp = Response(response=data, status = 200, mimetype="application/json")
